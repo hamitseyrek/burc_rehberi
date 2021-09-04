a3c7cf668b85ae275899a5146b8d5e1a2337ee8f
hamitseyrek <hamitseyrek@gmail.com>
2021-09-04 11:52:37 +0300

    create burc objects

--- burc_rehberi/lib/burc_list.dart
+++ burc_rehberi/lib/burc_list.dart
@@ -1,12 +1,35 @@
+import 'package:burc_rehberi/data/strings.dart';
+import 'package:burc_rehberi/model/burc.dart';
 import 'package:flutter/material.dart';
 
 class BurcList extends StatelessWidget {
-  const BurcList({ Key? key }) : super(key: key);
+    late List<Burc> allBurcs;
+
+   BurcList(){
+     allBurcs = setDatas();
+   }
+
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      appBar: AppBar(title: Text('Burç Listesi')),
+      body: Center(
+        child: ListView.builder(itemBuilder: (context,index){
+          return Container(
+
+          );
+        }, itemCount: allBurcs.length,),
+      ),
     );
+  }
+
+  List<Burc> setDatas() {
+    List<Burc> _temp = [];
+    for (var i = 0; i < 12; i++) {
+      Burc addBurc = Burc(Strings.BURC_ADLARI[i],Strings.BURC_TARIHLERI[i],Strings.BURC_GENEL_OZELLIKLERI[i],Strings.BURC_ADLARI[i].toLowerCase()+'_buyuk'+(i+1).toString()+'.png',Strings.BURC_ADLARI[i].toLowerCase()+(i+1).toString()+'.png');
+      _temp.add(addBurc);
+    }
+    return _temp;
   }
 }
